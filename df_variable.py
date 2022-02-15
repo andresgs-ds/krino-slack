2d567a9c8011126a380b459dd51837175ad73b0a
andrewq00 <andres.guinazu1601@alumnos.ubiobio.cl>
2022-02-15 11:15:07 -0300

    actualizacion base de datos 15-02-2022

--- df_variable.py
+++ df_variable.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 # -- Import and clean data (importing csv into pandas)
-df = pd.read_csv("data_clean_csv/data_slack_2022-02-11_11:14:11.csv")
-df_old = pd.read_csv("data_clean_csv/data_slack_2022-02-09_00:22:41.csv")
+df = pd.read_csv("data_clean_csv/data_slack_2022-02-15_11:13:47.csv")
+df_old = pd.read_csv("data_clean_csv/data_slack_2022-02-11_11:14:11.csv")
