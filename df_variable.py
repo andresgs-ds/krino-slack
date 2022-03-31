d7581bb274f45e0caef5979a96960b897b173212
adrewq00 <andres.guinazu1601@alumnos.ubiobio.cl>
2022-03-31 12:52:43 -0300

    Actualizacion BBDD 3-31-2022

--- df_variable.py
+++ df_variable.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 # -- Import and clean data (importing csv into pandas)
-df = pd.read_csv("data_clean_csv/data_slack_2022-03-16_00:14:54.csv")
-df_old = pd.read_csv("data_clean_csv/data_slack_2022-02-23_01:15:49.csv")
+df = pd.read_csv("data_clean_csv/data_slack_2022-03-31_12:35:53 - data_slack_2022-03-31_12:35:53.csv")
+df_old = pd.read_csv("data_clean_csv/data_slack_2022-03-16_00:14:54.csv")
